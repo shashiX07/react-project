6cbe395c40150ffcdaaaf987269ddb67843692e4
shashix07 <shashi007.iitkgp@gmail.com>
2025-03-07 17:29:46 +0530

    ui changes

--- currency-converter/src/App.tsx
+++ currency-converter/src/App.tsx
@@ -28,7 +28,7 @@ export default function App() {
   };
 
   return (
-    <div className="w-full h-screen flex flex-wrap justify-center items-center bg-blue-600">
+    <div className="w-full h-screen flex flex-wrap justify-center items-center bg-green-400">
       <div className="w-full">
         <div className="w-full max-w-md mx-auto border border-gray-200 rounded-lg p-5 backdrop-blur-sm bg-white/30">
           <form
